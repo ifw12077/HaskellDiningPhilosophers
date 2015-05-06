2e29c854bdf8a5189ca788f60440beec6687eec4
Gogetta <jopitz@hm.edu>
2015-05-06 05:20:10 -0700

    added table

--- src/Main.hs
+++ src/Main.hs
@@ -11,19 +11,19 @@ main = do
 
     numberOfSeats <- prompt "How many seats?"
 
-    let philosophers = map (("Philosopher " ++) . show) [1..numberOfPhilosophers]
-
     forks <- mapM newFork [1..numberOfSeats]
 
     let forkPairs = zip forks $ tail . cycle $ forks
 
     seats <- mapM newSeat $ zip [1..numberOfSeats] forkPairs
 
-    let namedPhilosophers  = map runPhilosopher philosophers
+    let philosophers        = map (("Philosopher " ++) . show) [1..numberOfPhilosophers]
+        namedPhilosophers   = map runPhilosopher philosophers
+        philosophersAtTable = zipWith ($) namedPhilosophers seats
 
     putStrLn "Running the philosophers. Press enter to quit."
 
-    startPhilosophers namedPhilosophers
+    startPhilosophers philosophersAtTable
 
     -- All threads exit when the main thread exits.
     getLine
